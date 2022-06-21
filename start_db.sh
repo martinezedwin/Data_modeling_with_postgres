6f77a4d50c32729495d0585c92d5b6340a01e725
Edwin Martinez <martinez92edwin@gmail.com>
2022-06-21 08:48:37 -0700

    added sqls

---
+++ start_db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# mkdir -p /tmp/postgresql
+docker stop postgresql
+docker rm postgresql
+docker run -d \
+    --name postgresql \
+    -e POSTGRES_USERNAME=student \
+    -e POSTGRES_PASSWORD=student \
+    -e POSTGRES_DBNAME=studentdb \
+    -p 5432:5432 \
+    frodenas/postgresql
